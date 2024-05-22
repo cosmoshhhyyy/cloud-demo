004d976826ba042ffedada20bcd4e2f99b18ea02
lpy <2552696563@qq,com>
2024-05-22 22:48:17 +0800

    服务远程调用完成

--- order-service/src/main/java/cn/itcast/order/service/OrderService.java
+++ order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -2,8 +2,12 @@ package cn.itcast.order.service;
 
 import cn.itcast.order.mapper.OrderMapper;
 import cn.itcast.order.pojo.Order;
+import cn.itcast.order.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
 
 @Service
 public class OrderService {
@@ -11,9 +15,17 @@ public class OrderService {
     @Autowired
     private OrderMapper orderMapper;
 
+    @Resource
+    private RestTemplate restTemplate;
+
     public Order queryOrderById(Long orderId) {
         // 1.查询订单
         Order order = orderMapper.findById(orderId);
+        // 2.查询用户，远程调用
+        String url = "http://localhost:8081/user/" + order.getUserId();
+        User user = restTemplate.getForObject(url, User.class);
+        // 3. 封装
+        order.setUser(user);
         // 4.返回
         return order;
     }
